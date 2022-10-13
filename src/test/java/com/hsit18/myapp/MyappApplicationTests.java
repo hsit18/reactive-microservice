2378a23aef90e84a864f860463c29886b7e0b1e0
Harpreet Singh <hsit18@gmail.com>
2022-10-14 00:47:53 +0530

    Initial setup

---
+++ src/test/java/com/hsit18/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsit18.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
